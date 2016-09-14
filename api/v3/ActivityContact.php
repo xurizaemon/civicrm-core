743cf00a3436baf2717993cac656e8041c5722e3
Chris Burgess <chris@giantrobot.co.nz>
2016-09-14 23:59:06 +1200

    CRM-19365 WIP. Does not work ("Mandatory key(s) missing from params array: id").

--- api/v3/ActivityContact.php
+++ api/v3/ActivityContact.php
@@ -57,15 +57,58 @@ function _civicrm_api3_activity_contact_create_spec(&$params) {
 }
 
 /**
+ * Adjust Metadata for Delete action.
+ *
+ * The metadata is used for setting defaults, documentation & validation.
+ *
+ * @param array $params
+ *   Array of parameters determined by getfields.
+ */
+function _civicrm_api3_activity_contact_delete_spec(&$params) {
+  // Is this required, if we manually check parameters in the actual
+  // delete function? If so, how do I specify the accepted parameters?
+}
+
+/**
  * Delete an existing ActivityContact record.
  *
  * @param array $params
  *
  * @return array
- *   API result array
+ *   Api Result
  */
 function civicrm_api3_activity_contact_delete($params) {
-  return _civicrm_api3_basic_delete(_civicrm_api3_get_BAO(__FUNCTION__), $params);
+  // Accept a delete which provides any one (or more) of these parameters.
+  $accepted_params = array(
+    'id',
+    'activity_id',
+    'contact_id',
+    'record_type_id',
+  );
+
+  // Accept delete by ID, activity_id, contact_id, or
+  // combination.
+  //
+  // (You might also want to filter by record_type_id.)
+  civicrm_api3_verify_one_mandatory($params, NULL, $accepted_params);
+
+  $dao = new CRM_Activity_BAO_ActivityContact();
+  foreach ($accepted_params as $key) {
+    if (isset($params[$key])) {
+      $dao->$key = $params[$key];
+    }
+  }
+  if ($dao->find()) {
+    while ($dao->fetch()) {
+      $dao->delete();
+    }
+    return civicrm_api3_create_success();
+  }
+  else {
+    throw new API_Exception('Could not delete entity id ' . $params['id']);
+  }
+
+  throw new API_Exception('no delete method found');
 }
 
 /**
